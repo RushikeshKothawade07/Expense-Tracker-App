30d106456da22bf234c275c2992660e5bb039090
Rushikesh Kothawade <rushikeshsk2105@gmail.com>
2023-03-14 02:13:30 +0530

    Initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
+import React from "react";
+import "./App.css";
 
-import React from 'react'
-import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Expense Tracker App</h1>
-    </div>
-  )
-}
+    <div className="App">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
+          Expense Tracker
+        </h1>
 
-export default App
+        {/* grid columns */}
+        <div className="grid md:grid-cols-2 gap-4">
+  
+    
+      
+     
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
